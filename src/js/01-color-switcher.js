ea189624486568662814fe8592dcb7f67ae8ce1e
Oleksandra <photoliya@gmail.com>
2023-05-20 20:06:19 +0300

    task1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,24 @@
+const startButton = document.querySelector('button[data-start]');
+const stopButton = document.querySelector('button[data-stop]');
+
+let timer;
+
+startButton.addEventListener('click', () => {
+    timer = setInterval(() => {
+       document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+
+    startButton.disabled = true;
+});
+
+stopButton.addEventListener('click', () => {
+    clearInterval(timer);
+
+    startButton.disabled = false;
+});
+
+
+function getRandomHexColor() {
+    return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+  }
 
